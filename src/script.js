df67a11973ef3ff336773d0980b9326f285fa3c9
JuliaRocha99 <59797584+JuliaRocha99@users.noreply.github.com>
2020-04-08 18:07:49 +0100

    Remove console log

--- src/script.js
+++ src/script.js
@@ -1,62 +1,16 @@
 function displayTemperature(response) {
-  console.log(temperature);
-
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
-
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 let apiKey = "bf0050d8d22310df394fff19194582c3";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
-
-//   $.getJSON(
-//     `http://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=bf0050d8d22310df394fff19194582c3&units=metric`;
-//     function (data) {
-//       city.innerHTML = searchCity;
-//       celsiusTemperature.innerHTML = Math.round(response.data.main.temp);
-//       outputFahrenheit.innerHTML = Math.round(
-//         convertToF(convertKelvinToCelsius(data.main.temp))
-//       );
-//     }
-//   );
-// }
-
-// function showForecast(position) {
-//   let city = document.querySelector("#city");
-//   let apikey = "bf0050d8d22310df394fff19194582c3";
-//   let lat = position.coords.latitude;
-//   let lon = position.coords.longitude;
-//   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}`;
-//   $.get(
-//     "http://ipinfo.io",
-//     function (response) {
-//       city.innerHTML = response.city;
-//     },
-//     "jsonp"
-//   );
-//   $.getJSON(url, function (data) {
-//     let celsiusTemperature = document.querySelector("#celsius-temperature");
-//     let outputFahrenheit = document.querySelector("#outputFahrenheit");
-//     celsiusTemperature.innerHTML = Math.round(
-//       convertKelvinToCelsius(data.main.temp)
-//     );
-//     outputFahrenheit.innerHTML = Math.round(
-//       convertToF(convertKelvinToCelsius(data.main.temp))
-//     );
-//   });
-// }
-
-// function currentLocation() {
-//   if (navigator.geolocation) {
-// //     navigator.geolocation.getCurrentPosition(showWeather);
-// //   } else {
-// //     alert("Geolocation not supported in this browser");
-// //   }
-// }
