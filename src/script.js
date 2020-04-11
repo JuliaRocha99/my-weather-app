275d7a04b2fc3ad2c5bed9f56ad09536b7c5c09d
JuliaRocha99 <59797584+JuliaRocha99@users.noreply.github.com>
2020-04-11 15:06:16 +0100

    javascript to search engine

--- src/script.js
+++ src/script.js
@@ -16,8 +16,19 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let apiKey = "bf0050d8d22310df394fff19194582c3";
-let city = "Porto";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "bf0050d8d22310df394fff19194582c3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let searchCityElement = document.querySelector("#searchCity");
+  search(searchCityElement.value);
+}
+
+search("Porto");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
